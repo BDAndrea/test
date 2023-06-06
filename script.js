16a716c3cf7a78865092c12cc1a969680638f9f8
BDAndrea <bakovic.a@gmail.com>
2023-06-06 19:42:47 +0200

    Verzija 1

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("V 01");
+console.log("V 02");
+console.log("V 03");
+
+// git config --global user.email "example@example.com"
+// treba biti isto kao github name (preporuka je, ne mora)
+// git config --global user.name "UserName"
+// git add . - za dodavanje
+// git log
+// git commit -m "Verzija 1"
+// git commit -m "Verzija 1"--amend ispravlja postojecu verziju
+// git checkout nekibrojevicommita
