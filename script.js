b6f8643907b1c7145aadeec458ed7d8f35fd76ab
BDAndrea <bakovic.a@gmail.com>
2023-06-06 19:44:59 +0200

    Verzija 2

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 
 console.log("V 01");
 console.log("V 02");
-console.log("V 03");
 
 // git config --global user.email "example@example.com"
 // treba biti isto kao github name (preporuka je, ne mora)
 // git config --global user.name "UserName"
 // git add . - za dodavanje
-// git log
+// git log - moze vidjeti samo prijasnje verzije (ako se prebacimo ize verzije 3 u verziju 2, onda on vidi samo verziju 2 i one prije) ako opet zelimo vidjeti sve verzije, i te poslije pisemo git log all
 // git commit -m "Verzija 1"
 // git commit -m "Verzija 1"--amend ispravlja postojecu verziju
 // git checkout nekibrojevicommita
